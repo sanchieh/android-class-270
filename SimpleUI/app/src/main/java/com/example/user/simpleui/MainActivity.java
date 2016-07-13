33ec8dc42a5ea396a6b1346f009061c90c039cb9
sanchieh <lucy84265@gmail.com>
2016-07-13 11:52:31 +0800

    refine layout

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -4,19 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     EditText editText;
     RadioGroup radioGroup;
+    ListView listView;
+    Spinner spinner;
 
-    String selectedSex = "男";
+    String selectedTea = "blcak tea";
+
+    List<Order> orders = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView)findViewById(R.id.textView);
         editText = (EditText)findViewById(R.id.editText);
         radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        listView = (ListView)findViewById(R.id.listView);
+        spinner = (Spinner)findViewById(R.id.spinner);
 
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
@@ -41,27 +54,49 @@ public class MainActivity extends AppCompatActivity {
         });
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(checkedId == R.id.maleRadioButton)
-                {
-                    selectedSex = "男";
-                }
-                else if (checkedId == R.id.femaleRadioButton)
-                {
-                    selectedSex = "女";
-                }
+                                                  @Override
+                                                  public void onCheckedChanged(RadioGroup group, int checkedId) {
+                                                      RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+                                                      selectedTea = radioButton.getText().toString();
+                                                  }
+                                              });
 
+            setupListView();
+
+            setupSpinner();
+        }
+
+            public void setupListView(){
+
+                OrderAdapter adapter = new OrderAdapter(this, orders);
+                listView.setAdapter(adapter);
             }
-        });
-    }
+
+            public void setupSpinner()
+            {
+                String[] data = getResources().getStringArray(R.array.storeInfos);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, data);
+                spinner.setAdapter(adapter);
+            }
+
+
+
+
 
     public void submit(View view)
     {
         String text = editText.getText().toString();
 
-        text = text + " 性別: "+selectedSex;
         textView.setText(text);
+
+        Order order = new Order();
+        order.note = text;
+        order.drinkName = selectedTea;
+        order.storeInfo = (String)spinner.getSelectedItem();
+
+        orders.add(order);
+
+        setupListView();
 
         editText.setText("");
     }
